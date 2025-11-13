d40b14a77fc925b266897aae02a8fce76fe82137
Sofia Andrade Petruk <sofia.petruk@castecnologia.com.br>
2025-11-12 21:25:21 -0300

    Criação das classes de segurança

---
+++ src/main/java/br/com/fiap/SoulBalance/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package br.com.fiap.SoulBalance.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService,
+                                                         PasswordEncoder passwordEncoder) {
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+
+        return provider;
+    }
+
+
+}
